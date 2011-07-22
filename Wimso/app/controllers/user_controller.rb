b78bfc3e5b5b710f1304f63155102aa914cc0323
Schippi <Peter@localhost.gov>
2011-07-22 14:49:24 +0200

    double friends removed

--- Wimso/app/controllers/user_controller.rb
+++ Wimso/app/controllers/user_controller.rb
@@ -11,8 +11,16 @@ class UserController < ApplicationController
 	def add_friend
 		puts "----------------------------"
 		puts params[:id]
-		User.find(current_user.id).friends << User.find(params[:id])
-		redirect_to show_profile_path
+		begin
+		(User.find(current_user.id).friends << User.find(params[:id]))
+		
+		rescue
+			flash[ :notice] = "already in list"
+			redirect_to show_profile_path current_user.id
+			
+			return
+		end
+			redirect_to show_profile_path
 		
 	end
 	
