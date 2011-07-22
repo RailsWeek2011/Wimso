ca131d1bffe4ce682e1e48d3c743eedc92d3d3e1
Okarin <jens.mehler@mni.th-mittelhessen.de>
2011-07-22 11:19:16 +0200

    devise merge and improved show_user

--- Wimso/db/seeds.rb
+++ Wimso/db/seeds.rb
@@ -10,3 +10,9 @@ u1=User.create :name => "eins", :email => "foo1@bar.gov", :password => "123456",
 u2=User.create :name => "zwei", :email => "foo2@bar.gov", :password => "123456", :password_confirmation => "123456", :privacy_flag => false
 u3=User.create :name => "drei", :email => "foo3@bar.gov", :password => "123456", :password_confirmation => "123456", :privacy_flag => false
 
+u1.friends << u2
+u2.friends << u1
+
+u1.save
+u2.save
+
