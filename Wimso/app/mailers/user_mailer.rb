28bfa31e84ca1f116c9326ed567142e92a57e261
Schippi <Peter@localhost.gov>
2011-07-28 11:09:04 +0200

    mailer imp

--- Wimso/app/mailers/user_mailer.rb
+++ Wimso/app/mailers/user_mailer.rb
@@ -5,13 +5,27 @@ class UserMailer < ActionMailer::Base
 
   
   def welcome(ur)
-    user = User.find(ur.user_id)
-    mail(:to => user.email,
-		:from => "carsten.schipmann@mni.thm.de",
-         :subject => "standard-test")
-UserMailer.delay(:run_at => 5.seconds.from_now).welcome(ur)
-	 
-	 
+	  begin
+		@user = User.find(25)
+		@userrun= ur
+		Run.find(@userrun.run_id)
+		
+		if(ur.is_nil? || @user.is_nil? )
+			mail(:to => "garbagegarbage@web.de",
+			:from => "carsten.schipmann@mni.thm.de",
+			:subject => "kaputt-test")	
+		else
+			#TODO last modified
+			mail(:to => @user.email,
+				:from => "carsten.schipmann@mni.thm.de",
+				:subject => "Wimso, Dein SerienUpdate")
+			#UserMailer.delay(:run_at => 5.seconds.from_now).welcome(ur)
+		end
+	rescue		
+		mail(:to => "garbagegarbage@web.de",
+				:from => "carsten.schipmann@mni.thm.de",
+				:subject => "kaputt")
+	 end
   end
   
   
