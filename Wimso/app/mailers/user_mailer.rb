9b8444e9c2610aa86eb294e93ec9770b4fab4ddf
Okarin <jens.mehler@mni.th-mittelhessen.de>
2011-07-28 13:12:42 +0200

    improved dashboard

---
+++ Wimso/app/mailers/user_mailer.rb
@@ -0,0 +1,33 @@
+class UserMailer < ActionMailer::Base
+  
+  #config.action_mailer.delivery_method = :smtp 						
+								
+
+  
+  def welcome(ur)
+	  begin
+		@user = User.find(25)
+		@userrun= ur
+		Run.find(@userrun.run_id)
+		
+		if(ur.is_nil? || @user.is_nil? )
+			mail(:to => "garbagegarbage@web.de",
+			:from => "carsten.schipmann@mni.thm.de",
+			:subject => "kaputt-test")	
+		else
+			#TODO last modified
+			mail(:to => @user.email,
+				:from => "carsten.schipmann@mni.thm.de",
+				:subject => "Wimso, Dein SerienUpdate")
+			#UserMailer.delay(:run_at => 5.seconds.from_now).welcome(ur)
+		end
+	rescue		
+		mail(:to => "garbagegarbage@web.de",
+				:from => "carsten.schipmann@mni.thm.de",
+				:subject => "kaputt")
+	 end
+  end
+  
+  
+  
+end
